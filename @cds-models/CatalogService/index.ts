0115ac9e87f181e86fd2801535460209bedb7a73
Ludwig Stockbauer-Muhr <stocki87@gmail.com>
2024-05-01 22:27:28 +0200

    chore: update cds-typer

--- @cds-models/CatalogService/index.ts
+++ @cds-models/CatalogService/index.ts
@@ -4,12 +4,12 @@ import * as __ from './../_';
 export default { name: 'CatalogService' }
 export function _BookAspect<TBase extends new (...args: any[]) => object>(Base: TBase) {
   return class Book extends Base {
-        createdAt?: string | null;
+        createdAt?: __.CdsTimestamp | null;
     /**
     * Canonical user ID
     */
         createdBy?: _.User | null;
-        modifiedAt?: string | null;
+        modifiedAt?: __.CdsTimestamp | null;
     /**
     * Canonical user ID
     */
@@ -17,12 +17,12 @@ export function _BookAspect<TBase extends new (...args: any[]) => object>(Base: 
         ID?: string;
         title?: string | null;
         stock?: number | null;
-        publishedOn?: string | null;
-      static actions: {
-      }
+        publishedOn?: __.CdsDate | null;
+      static readonly actions: Record<never, never>
   };
 }
 export class Book extends _._managedAspect(_._cuidAspect(_BookAspect(__.Entity))) {static drafts: typeof Book}
 Object.defineProperty(Book, 'name', { value: 'CatalogService.Books' })
-export class Books extends Array<Book> {static drafts: typeof Book}
+export class Books extends Array<Book> {static drafts: typeof Book
+$count?: number}
 Object.defineProperty(Books, 'name', { value: 'CatalogService.Books' })
--- @cds-models/my/bookshop/index.ts
+++ @cds-models/my/bookshop/index.ts
@@ -5,12 +5,12 @@ export function _BookAspect<TBase extends new (...args: any[]) => object>(Base: 
   return class Book extends Base {
         title?: string | null;
         stock?: number | null;
-        publishedOn?: string | null;
-      static actions: {
-      }
+        publishedOn?: __.CdsDate | null;
+      static readonly actions: Record<never, never>
   };
 }
 export class Book extends _._managedAspect(_._cuidAspect(_BookAspect(__.Entity))) {static drafts: typeof Book}
 Object.defineProperty(Book, 'name', { value: 'my.bookshop.Books' })
-export class Books extends Array<Book> {static drafts: typeof Book}
+export class Books extends Array<Book> {static drafts: typeof Book
+$count?: number}
 Object.defineProperty(Books, 'name', { value: 'my.bookshop.Books' })
