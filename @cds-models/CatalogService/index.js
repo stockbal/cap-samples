9b60f2faaaad58415a20a04ad464fac08352cf48
Ludwig Stockbauer-Muhr <stocki87@gmail.com>
2024-09-13 10:19:38 +0200

    chore: remove generated types from git

--- @cds-models/CatalogService/index.js
+++
@@ -1,9 +0,0 @@
-// This is an automatically generated file. Please do not change its contents manually!
-const cds = require('@sap/cds')
-const csn = cds.entities('CatalogService')
-module.exports = { name: 'CatalogService' }
-module.exports.Book = csn.Books
-module.exports.Books = csn.Books
-// events
-// actions
-// enums
--- @cds-models/my/bookshop/index.js
+++
@@ -1,8 +0,0 @@
-// This is an automatically generated file. Please do not change its contents manually!
-const cds = require('@sap/cds')
-const csn = cds.entities('my.bookshop')
-module.exports.Book = csn.Books
-module.exports.Books = csn.Books
-// events
-// actions
-// enums
