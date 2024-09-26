110a18b834a095e31f78d69535fd2c1cdac321af
Ludwig Stockbauer-Muhr <Ludwig.Stockbauer-Muhr@msg.group>
2024-09-26 23:39:42 +0200

    chore: add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  extends: ["eslint:recommended", "plugin:prettier/recommended", "plugin:@typescript-eslint/recommended"],
+  plugins: ["prettier", "@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2022,
+    project: "./tsconfig.eslint.json",
+    tsconfigRootDir: __dirname
+  },
+  ignorePatterns: ["node_modules/", "gen/", "mta_archives/", "@cds-models/", "coverage/"],
+  env: {
+    es2022: true,
+    node: true,
+    jest: true,
+    mocha: true
+  },
+  globals: {
+    SELECT: true,
+    INSERT: true,
+    UPSERT: true,
+    UPDATE: true,
+    DELETE: true,
+    CREATE: true,
+    DROP: true,
+    CDL: true,
+    CQL: true,
+    CXL: true,
+    cds: true
+  },
+  rules: {
+    "no-console": "off",
+    "@typescript-eslint/no-floating-promises": "error",
+    "@typescript-eslint/no-misused-promises": "error",
+    "require-atomic-updates": "off"
+  }
+};
--- srv/server.ts
+++ srv/server.ts
@@ -2,11 +2,12 @@ import * as CatalogService from "#cds-models/CatalogService";
 import { Books } from "#cds-models/my/bookshop";
 import cds from "@sap/cds";
 
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
 cds.on("served", async () => {
   // sample select to see top level import works
   console.table(await SELECT.one.from(Books));
 
   const service = await cds.connect.to(CatalogService.default);
 
-  await service.run(SELECT.from(CatalogService.Books));
+  await service.run(SELECT.from(CatalogService.Books).columns((b) => b("*")));
 });
